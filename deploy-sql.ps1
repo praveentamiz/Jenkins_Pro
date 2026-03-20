076e9cffa559f8f2d5eafe561aca0f2e0b0e69bd
praveentamiz <139312759+praveentamiz@users.noreply.github.com>
2026-03-20 10:08:59 +0530

    Update deploy-sql.ps1

--- deploy-sql.ps1
+++ deploy-sql.ps1
@@ -1,27 +1,15 @@
-$sqlServer = $env:SQL_SERVER
-$database  = $env:DATABASE
-$sqlFolder = $env:SQL_FOLDER
-
 Write-Host "SQL Deployment Started"
 
-if (!(Test-Path $sqlFolder)) {
-    Write-Host "SQL folder not found!"
-    exit 1
-}
+$server = "localhost"
+$database = "TEST_DB"
 
-$sql = "IF DB_ID('$database') IS NULL CREATE DATABASE [$database]"
-sqlcmd -S $sqlServer -Q $sql -E
+Get-ChildItem -Path ".\SQLFiles\*.sql" | ForEach-Object {
+    Write-Host "Executing: $($_.Name)"
 
-$files = Get-ChildItem -Path $sqlFolder -Filter *.sql | Sort-Object Name
-
-foreach ($file in $files) {
-    Write-Host "Executing: $($file.Name)"
-    sqlcmd -S $sqlServer -d $database -E -i $file.FullName
+    sqlcmd -S $server -d $database -E -i $_.FullName -Encrypt Yes -TrustServerCertificate Yes
 
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "Error in $($file.Name)"
+        Write-Host "Error in $($_.Name)"
         exit 1
     }
 }
-
-Write-Host "SQL Deployment Completed"
